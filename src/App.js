bc80a42abcac25b24ff2331bdd164512c1f35fe1
Hyormone <ddf998@gmail.com>
2023-01-31 23:36:42 +0900

    Feat: App.js 컴포넌트 렌더링 && index.css 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import AppHeader from "./components/Common/AppHeader";
+import AppFooter from "./components/Common/AppFooter";
+import AppMain from "./components/Main/AppMain";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
     </div>
   );
 }
